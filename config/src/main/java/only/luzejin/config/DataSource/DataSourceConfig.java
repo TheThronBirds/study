615186e030af55c5bf8cbf5bfee817ea24a5343a
luzejin <luzejin@yhfin.com>
2020-01-07 20:30:15 +0800

    文件修改

--- config/src/main/java/only/luzejin/config/DataSource/DataSourceConfig.java
+++ config/src/main/java/only/luzejin/config/DataSource/DataSourceConfig.java
@@ -1,7 +1,15 @@
 package only.luzejin.config.DataSource;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 包名称：only.luzejin.config.DataSource
@@ -12,6 +20,43 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class DataSourceConfig {
+
+    @Value("only.study.profile: company")
+    private String profile;
+
+    @Bean("homeDataSource")
+    @ConfigurationProperties("only.study.homeDataSource")
+    public DataSource getHomeDataSource(){
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setMinimumIdle(10);
+        dataSource.setMaximumPoolSize(20);
+        return dataSource;
+    }
+
+    @Bean("companyDataSource")
+    @ConfigurationProperties("only.study.companyDataSource")
+    public DataSource getCompanyDataSource(){
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setMinimumIdle(10);
+        dataSource.setMaximumPoolSize(20);
+        return dataSource;
+    }
+
+
+    @Bean("dynamicDataSource")
+    public DataSource getDynamicDatSource(@Qualifier("homeDataSource") DataSource homeDataSource,@Qualifier("companyDataSource") DataSource companyDataSource){
+        Map<Object,Object> targetDataSource = new HashMap<Object, Object>();
+        targetDataSource.put(DataSourceType.DataBaseType.Home,homeDataSource);
+        targetDataSource.put(DataSourceType.DataBaseType.Company,companyDataSource);
+        DynamicDataSource dataSource = new DynamicDataSource();
+        dataSource.setTargetDataSources(targetDataSource);
+        if(profile.equals("company")){
+            dataSource.setDefaultTargetDataSource(companyDataSource);
+        }else{
+            dataSource.setDefaultTargetDataSource(homeDataSource);
+        }
+        return dataSource;
+    }
 
 
 
--- config/src/main/java/only/luzejin/config/DataSource/DynamicDataSource.java
+++ config/src/main/java/only/luzejin/config/DataSource/DynamicDataSource.java
@@ -1,5 +1,7 @@
 package only.luzejin.config.DataSource;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.jdbc.datasource.AbstractDataSource;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
@@ -10,7 +12,10 @@ import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
  * 创建人：@author luzejin
  * 创建时间：2020-1-3 18:02
  */
+@Getter
+@Setter
 public class DynamicDataSource extends AbstractRoutingDataSource {
+
     @Override
     protected Object determineCurrentLookupKey() {
         return DataSourceType.getDataBaseType();
